6ce9cf907587e2b1753657ef2cbb026a2c55c8ed
Pensezy <pensezy.si@gmail.com>
2025-01-31 13:06:51 +0100

    Update Travail.py

--- Travail.py
+++ Travail.py
@@ -1,7 +1,7 @@
 #importation de librairie
 import pandas as pd
 import seaborn as sns
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 #chargement de la base de donnée
 df=pd.read_excel('Iris.xlsx')
