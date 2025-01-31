00a6d48d6c81961050436507c4d89c710291e849
Pensezy <pensezy.si@gmail.com>
2025-01-31 12:33:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sklearn.linear_model import LinearRegression
 # Charger les données
 data = pd.read_excel("Iris.xlsx")
 
+print("Bonjour pensezy")
+"""
 # Charger le modèle
 model = LinearRegression()
 model.fit(data[['feature1', 'feature2']], data['target'])
@@ -19,3 +21,4 @@ feature2 = st.slider("Feature 2", min_value=0, max_value=100)
 if st.button("Prédire"):
     prediction = model.predict([[feature1, feature2]])
     st.write(f"Prédiction : {prediction[0]}")
+"""
