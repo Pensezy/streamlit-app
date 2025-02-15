fd9c359f95eb427920d629c320d9999e15e5ee4a
Pensezy <pensezy.si@gmail.com>
2025-02-15 18:14:59 +0100

    Update Etape.py

--- Etape.py
+++ Etape.py
@@ -43,3 +43,49 @@ knn=KNeighborsClassifier(n_neighbors=5)#3
 #2.
 #Entrainons le modèle 
 knn.fit(X_train, y_train)
+
+
+#Etape 5: Evaluer le modèle
+
+#1.
+#Prédire les classes de l'ensemble de test
+y_pred=knn.predict(X_test)
+
+# 2.Affichons la matrice de confusion 
+conf_matrix = sm.confusion_matrix (y_test, y_pred)
+
+sns.heatmap(conf_matrix, annot=True, cmap='Blues', fmt='d', xticklabels=df['Species'].unique(), yticklabels=df['Species'].unique())
+plt.xlabel('Prédictions')
+plt.ylabel('Vraies classes')
+plt.show()
+
+#3.
+#Calculer l'exactitude
+accuracy = sm.accuracy_score(y_test, y_pred)
+print(f"Exactitude du modèle: {accuracy*100:.2f}%")
+
+#Afficher le rapport de classification 
+print("Rapport de classification : \n", sm.classification_report(y_test, y_pred))
+
+
+#Etape 6
+#1. Analysons les résultats du modèle 
+precision=sm.precision_score(y_test, y_pred, average='weighted')
+rappel=sm.recall_score(y_test, y_pred, average='weighted')
+f1=sm.f1_score(y_test,y_pred,average='weighted')
+#Calculons les positifs
+TN=conf_matrix[0,0] #Les éléments de la diagonale principale : représentent les prédictions correctes
+FP= conf_matrix[0,1] #Les éléments au dessus de la diagonale principale : représentent les prédictions incorrect (faux positifs)
+FN=conf_matrix[1,0]#Les éléments en dessous de la diagonale principale : représentent les prédictions incorrectes (Faux négatifs)
+TP=conf_matrix[1,1]#Les éléments en dehors de la diagonale principale : représentent les prédictions incorrect (Vrai négatifs)
+
+erreur=(FP + FN)/(TP+FP + FN + TN)
+print("Précision :", precision)
+print("Rappel :", rappel)
+print("Valeur F1 :", f1)
+print("Erreur :", erreur)
+
+plt.imshow(conf_matrix,interpolation='nearest')
+plt.title("Matrice de confusion")
+plt.colorbar()
+plt.show()
